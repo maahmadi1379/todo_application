3be4129b3ad35d4618f4b11bc03e5d2a55ae6982
ahmadi <ahmadi@zhinogroup.com>
2024-06-19 23:54:26 +0330

    :sparkles: feat: add admin panel configuration

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Task, Note
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'status', 'created', 'updated', 'deleted', 'is_deleted')
+    list_filter = ('status', 'is_deleted')
+    ordering = ('created', )
+
+
+@admin.register(Note)
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'description', 'created', 'updated', 'deleted', 'is_deleted')
+    list_filter = ('is_deleted', )
+    ordering = ('created', )
